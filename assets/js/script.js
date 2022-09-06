050d64bd3f8518f125677dba93c63dbf9833d7cd
Chrisfaherty <chrismactom@gmail.com>
2022-09-06 19:34:26 +0000

    Edit the Itags on the buttons so that you can click anywhere on the button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,16 @@
 //Selectors
 const shoppingListInput = document.querySelector('.shopping-list-input');
 const shoppingListButton = document.querySelector('.shopping-list-button');
-const sortShoppingListItems = document.querySelector('.shopping-items-list-item');
+const shoppingItemsListItem = document.querySelector('.shopping-items-list-item');
 const shoppingItemsList = document.querySelector('.shopping-items-list');
 
 //Event Listeners
 shoppingListButton.addEventListener('click', addItemToList);
 /*
 sortShoppingListItems.addEventListener('click', sortItemInList);
-shoppingItemsListItem.addEventListener('click', removeItemFromList);
 */
+shoppingItemsListItem.addEventListener('click', removeItemFromList);
+
 //Functions
 
 function addItemToList(addItem){
@@ -41,5 +42,15 @@ function addItemToList(addItem){
     addItemToListDiv.appendChild(removeItemButton);
 
     // Append Shopping List
-    sortShoppingListItems.appendChild(addItemToListDiv);
+    shoppingItemsListItem.appendChild(addItemToListDiv);
+}
+
+// Creating a function to delete the list Items.
+
+function removeItemFromList(additem){
+    const item = additem.target;
+    // deleting the list item
+    if (item.classList[0] === "remove-item-btn"){
+        item.remove();
+    }
 }
