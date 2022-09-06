f56b10cb3c8958ca1f549ab771f91bb85e74b553
Chrisfaherty <chrismactom@gmail.com>
2022-09-06 14:34:32 +0000

    Add selectors to the script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 //Selectors
+const shoppingListInput = document.querySelector('.shopping-list-input');
+const shoppingListButton = document.querySelector('shopping-list-button');
+const sortShoppingListItems = document.querySelector('sort-shopping-list-items');
+const shoppingItemsListItem = document.querySelector('shopping-items-list-items');
 
 //Event Listeners
+shoppingListButton.addEventListener('click', addItemToList);
+sortShoppingListItems.addEventListener('click', sortItemInList);
+shoppingItemsListItem.addEventListener('click', removeItemFromList);
 
 //Functions
