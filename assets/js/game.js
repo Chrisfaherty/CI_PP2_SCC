41ee05be9cc069deea6f60d3e9b59d0b2303f35f
Chrisfaherty <chrismactom@gmail.com>
2022-09-07 13:49:13 +0000

    Edit the function so that the button matches the image

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,12 +1,11 @@
 // Start of game.html JS.
 const buttons = document.getElementsByClassName('control');
-const mScore = document.getElementsByClassName('match');
-const dScore = document.getElementsByClassName('different');
-const humanImg = document.getElementsByClassName('human-img');
-const computerImg = document.getElementsByClassName('computer-img');
+const mScore = document.getElementById('matches');
+const dScore = document.getElementById('not-matched');
+const humanImg = document.getElementById('human-img');
+const computerImg = document.getElementById('computer-img');
 //create a random generator to select between the images at random
-const computerChoices = ['bottle', 'cake', 'choclate', 'coffee', 'cookie', 'ice-cream'];
-
+const choices = ['coffee', 'bottle', 'cake', 'choclate', 'cookie', 'ice-cream'];
 
     // This function is used to fade out the info screen and fade in the game
     const startGame = function (){
@@ -19,31 +18,27 @@ const computerChoices = ['bottle', 'cake', 'choclate', 'coffee', 'cookie', 'ice-
             playZone.classList.add('fadeIn'); 
         });
     };
+    startGame();
 
 // Adding event listeners to the buttons
-    for (let button of buttons) {
+   for (let button of buttons) {
         button.addEventListener("click", function(){
             let humanChoice = this.getAttribute("data-choice");
-            startGame(humanChoice);
+            playGame(humanChoice);
         });
     };
+ 
+// This function will be used to start the game
+   function playGame(humanChoice) {
 
-    // This function will be used to start the game
-    const playGame = function (){
+    humanImg.src = `./assets/images/${choices[humanChoice]}.png`
+    humanImg.alt = choices[humanChoice];
 
+    let computerChoice = Math.floor(Math.random() * 6);
 
-    
+    computerImg.src = `./assets/images/${choices[computerChoice]}.png`
+    computerImg.alt = choices[computerChoice];
 
-       /* // create a loop to go through the arrey
-        choices.forEach((choice) => {
-            choice.addEventListener('click', function(){
-        // For the computer to generate a random number from the arrey with no decimals
-        const computerNumber = Math.floor(Math.random() * 6);
-        const computerChoice = computerChoices[computerNumber];
-        console.log(computerChoice);
-            });
-        });*/
-    };
+   };
 
-    startGame();
-    playGame();
+   playGame();
