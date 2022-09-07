f4eed546e39e4fdd9bbf77f8a2b505cb17586862
Chrisfaherty <chrismactom@gmail.com>
2022-09-07 16:26:52 +0000

    Edit function fo the place holders apear before the game starts

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,7 +5,8 @@ const dScore = document.getElementById('not-matched');
 const humanImg = document.getElementById('human-img');
 const computerImg = document.getElementById('computer-img');
 //create a random generator to select between the images at random
-const choices = ['coffee', 'bottle', 'cake', 'choclate', 'cookie', 'ice-cream'];
+const choices = ['coffee', 'bottle', 'cake', 'chocolate', 'cookie', 'ice-cream'];
+
 
     // This function is used to fade out the info screen and fade in the game
     const startGame = function (){
@@ -23,22 +24,23 @@ const choices = ['coffee', 'bottle', 'cake', 'choclate', 'cookie', 'ice-cream'];
 // Adding event listeners to the buttons
    for (let button of buttons) {
         button.addEventListener("click", function(){
-            let humanChoice = this.getAttribute("data-choice");
+            let humanChoice = this.getAttribute("data-choice")
             playGame(humanChoice);
         });
     };
  
 // This function will be used to start the game
    function playGame(humanChoice) {
-
-    humanImg.src = `./assets/images/${choices[humanChoice]}.png`
+    const imageFileName = choices[humanChoice] ?? 'coffee'
+    humanImg.src = `./assets/images/${imageFileName}.png`
     humanImg.alt = choices[humanChoice];
 
     let computerChoice = Math.floor(Math.random() * 6);
 
     computerImg.src = `./assets/images/${choices[computerChoice]}.png`
     computerImg.alt = choices[computerChoice];
+            
+    };
 
-   };
 
-   playGame();
+
