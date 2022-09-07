64dbd818e55e3a8b5dee827426c68d9df7aeaa91
Chrisfaherty <chrismactom@gmail.com>
2022-09-07 11:04:11 +0000

    Edit button class so they can be randomly pulled

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,10 +24,26 @@ const game = function (){
         const computerImg = document.querySelector('.computer-img');
 
         //create a random generator to select between the images at random
-        const computerChoices = ['bottle','cake', 'choclate', 'coffee', 'cookie', 'ice-cream'];
-        const computerChoice = Math.floot(Math.random() * 6);
+        const computerChoices = ['bottle', 'cake', 'choclate', 'coffee', 'cookie', 'ice-cream'];
+        
+        for (let choice of computerChoices){
+            choice.addEventListener('click', function(){
+                const computerNumber = Math.floor(Math.random() * 6);
+                const computerChoice = computerChoices[computerNumber];
+                console.log(computerChoice); 
+            })
+        }
+
+       /* // create a loop to go through the arrey
+        choices.forEach((choice) => {
+            choice.addEventListener('click', function(){
+        // For the computer to generate a random number from the arrey with no decimals
+        const computerNumber = Math.floor(Math.random() * 6);
+        const computerChoice = computerChoices[computerNumber];
         console.log(computerChoice);
-    }
+            });
+        });*/
+    };
 
     startGame();
     playGame();
