d612f2b2e63f6d58ef2bd0f744d7f8cd4e33c0f4
Chrisfaherty <chrismactom@gmail.com>
2022-09-08 10:54:45 +0000

    Edit java script to update the score

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,14 @@
 // Start of game.html JS.
     const buttons = document.getElementsByClassName('control');
-    /*
-    const mScore = document.getElementById('matches');
-    const dScore = document.getElementById('not-matched');
-    */
     const humanImg = document.getElementById('human-img');
     const computerImg = document.getElementById('computer-img');
-    //create a random generator to select between the images at random
     const choices = ['coffee', 'bottle', 'cake', 'chocolate', 'cookie', 'ice-cream'];
-    
     let mScore = 0;
     let dScore = 0;
     
     
-        // This function is used to fade out the info screen and fade in the game
-        const startGame = function (){
+// Enter the game when you click lets play on the info screen
+    const startGame = function (){
             const startBtn = document.querySelector('.info button');
             const infoScreen = document.querySelector('.info');
             const playZone = document.querySelector('.play-zone');
@@ -23,18 +17,22 @@
                 infoScreen.classList.add('fadeOut');
                 playZone.classList.add('fadeIn'); 
             });
-        };
+    };
+    // Call the above function Start Game
+        startGame();
     
-    // Adding event listeners to the buttons
+// Adding event listeners to the buttons
        for (let button of buttons) {
             button.addEventListener("click", function(){
-                let humanChoice = this.getAttribute("data-choice")
-                playGame(humanChoice);
+            let humanChoice = this.getAttribute("data-choice")
+            playGame(humanChoice);
             });
         };
+
+
      
-    // This function will be used to start the game
-       function playGame(humanChoice) {
+// This function will be used to start the game
+    function playGame(humanChoice) {
 
         //Update Images 
         const imageFileName = choices[humanChoice] ?? 'coffee'
@@ -46,40 +44,37 @@
         computerImg.src = `./assets/images/${choices[computerChoice]}.png`
         computerImg.alt = choices[computerChoice];
 
+        // Compare images
         compareChoices(humanImg.src, computerImg.src);
 
         };
 
-        /*
         // This section is to update the score
         const updateScore = () =>{
             const humanScore = document.querySelector('.match p');
             const computerScore = document.querySelector('.different p');
             humanScore.textContent = mScore;
             computerScore.textContent = dScore;
-        }; */
+        };
     
         // This section is used to check if there is a match
+        
         const compareChoices =(humanChoice, computerChoice) => {
+        
             //Checking for a match
             const outcome = document.querySelector('.outcome');
+
             if(humanChoice === computerChoice){
                 outcome.textContent = "It is a match";
+                mScore++;
+                updateScore();
+                return;
+            } else {
+                outcome.textContent = "It is not a match"
+                dScore++;
+                updateScore();
                 return;
             };
-            // Check for coffee
-            if(humanChoice === "coffee"){
-                if(computerChoice === "coffee"){
-                    outcome.textContent = "It is a Match";
-                    return;
-                } else {
-                    outcome.textContent = "It is NOT a Match";
-                    return;
-                }
-            };
-
             };
     
-        startGame();
-        playGame();
     
