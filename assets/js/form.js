5cf9c1a0c46054449b9816917dd7813af5a7f39a
Chrisfaherty <chrismactom@gmail.com>
2022-09-09 11:33:21 +0000

    Edit form to gather feedback

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,6 +1,13 @@
-const sendFormButton = document.getElementById("submit-button");
-
-document.getElementById("subscribe-form").addEventListener("submit", function (event) {
-    event.preventDefault();
-    emailjs.init("user_ar8nvoepKsy2RQrM8");
-});
+function sendMail(params){
+    var tempParams = {
+    fname: document.getElementById('fname').value,
+    lname: document.getElementById('lname').value,
+    email: document.getElementById('email').value,
+    list_radio: document.getElementById('list_radio').value,
+    game_radio: document.getElementById('game_radio').value,
+};
+emailjs.send('service_3tbvdmq','template_psbp8aj',tempParams)
+.then(function(res){
+    console.log('success', res.status);
+})
+};
