9c1333e6a2295417ca7f01e3e1a66eff41b23f09
Chrisfaherty <chrismactom@gmail.com>
2022-09-11 18:54:22 +0000

    Fix the form to have an alert once a email is sent

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,16 +1,17 @@
 
 /* This function is to connect the form with EmailJS */
-function sendMail(params){
-    var tempParams = {
-    name: document.getElementById('to_name').value,
-    email: document.getElementById('email').value,
-    message: document.getElementById('message').value,
-    
-};
-emailjs.send('service_3tbvdmq','template_psbp8aj',tempParams)
-.then(function(res){
-    console.log('success', res.status);
-}
-);
-}
-(function(){emailjs.init("ar8nvoepKsy2RQrM8");})();
+const sendFormButton = document.getElementById("submit-button");
+
+            document.getElementById('subscribe-form').addEventListener('submit', function(event) {
+                event.preventDefault();
+                emailjs.init('ar8nvoepKsy2RQrM8');
+                sendFormButton.value = "Sending...";
+
+                emailjs.sendForm('service_3tbvdmq','template_psbp8aj', this)
+                    .then(function() {
+                        sendFormButton.value = "Send";
+                        alert('Email Sent Sucessfully')
+                    }, function(error) {
+                        console.log(JSON.stringify(error));
+                    });
+            });
