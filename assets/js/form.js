8b492cd89bf9d30026cbbdd5f4cd1dc821cf95c6
Chrisfaherty <chrismactom@gmail.com>
2022-09-11 17:01:40 +0000

    Remove script from the form.html and put it into the js code

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,9 +5,13 @@ function sendMail(params){
     name: document.getElementById('to_name').value,
     email: document.getElementById('email').value,
     message: document.getElementById('message').value,
+    
 };
 emailjs.send('service_3tbvdmq','template_psbp8aj',tempParams)
 .then(function(res){
     console.log('success', res.status);
-});
 }
+
+);
+}
+(function(){emailjs.init("ar8nvoepKsy2RQrM8");})();
